f6c82a601c2888e521d00319dfde6ce98f41d96a
lang <447111964@qq.com>
2016-04-18 18:56:01 +0800

    代码初始化

---
+++ 数据库表改列/DBTableHelper.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 数据库表改列
+{
+    public class DBTableHelper
+    {
+        private string SqlConnection;
+
+        public DBTableHelper(string _SqlConnection)
+        {
+            SqlConnection = _SqlConnection;
+        }
+
+        /// <summary>
+        /// 查询所有的数据库的数据库名称
+        /// </summary>
+        /// <returns></returns>
+        public List<string> GetDBName()
+        {
+            List<string> result = new List<string>();
+            string sql = "Select name from master..sysdatabases ";
+
+            DataTable dt = new SqlHelper(SqlConnection).ExecuteDataTable(sql);
+            foreach (DataRow row in dt.Rows)
+            {
+                result.Add(row["name"].ToString());
+            }
+            return result;
+        }
+
+
+        /// <summary>
+        /// 获取数据库所有的表名称
+        /// </summary>
+        /// <returns></returns>
+        public List<string> GetDbTable()
+        {
+            List<string> result = new List<string>();
+            string sql = "select * from sysobjects where xtype='u' ";
+
+            DataTable dt = new SqlHelper(SqlConnection).ExecuteDataTable(sql);
+            foreach (DataRow row in dt.Rows)
+            {
+                result.Add(row["name"].ToString());
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 获取表中所有的列
+        /// </summary>
+        /// <param name="TableName">表名称</param>
+        /// <returns></returns>
+        public List<string> GetTableColumn(string TableName)
+        {
+            List<string> result = new List<string>();
+            string sql = string.Format("	select * from sys.columns where object_id=object_id('[{0}]')", TableName);
+            DataTable dt = new SqlHelper(SqlConnection).ExecuteDataTable(sql);
+            foreach (DataRow row in dt.Rows)
+            {
+                result.Add(row["name"].ToString());
+            }
+            return result;
+        }
+
+    }
+}
---
+++ 数据库表改列/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 数据库表改列
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            // new Program().exSql("");//修改数据
+            new Program().exListDBSql();//扫描整个数据库修改列
+            //new Program().DropColumn("");//删除列
+            Console.WriteLine("ok");
+        }
+
+        public List<string> UpdateSql()
+        {
+            List<string> result = new List<string>();
+            string sql = " update [table] set [column]=null where [column]='暂无数据' ; ";
+            string sql1 = " update [table] set [column]=null where [column]='暂无价格' ; ";
+            string sql2 = " update [table] set [column]=null where [column]='暂无'  ;";
+            string sql3 = " update [table] set [column]=null where [column]='暂无资料'  ;";
+            string sql4 = " update [table] set [column]=null where [column]='暂无均价'  ;";
+            string sql5 = " update [table] set [column]=replace([column],'&#183;','·') where [column] like '%&#183;%'  ;";
+
+            result.Add(sql);
+            result.Add(sql1);
+            result.Add(sql2);
+            result.Add(sql3);
+            result.Add(sql4);
+            result.Add(sql5);
+            return result;
+        }
+
+        public void exListDBSql()
+        {
+            List<string> listDb = new DBTableHelper("").GetDBName();
+            foreach (var item in listDb)
+            {
+                //List<string> list = new List<string>();
+                //list.Add("宝鸡");
+                //list.Add("保定");
+                //list.Add("滨州");
+                //list.Add("沧州");
+                //list.Add("常德");
+
+                if (item.Contains("TempDB_"))
+                {
+                    string SqlConnection = "Data Source=192.168.11.202;Initial Catalog =[DbName];User Id = sa;Password = gh001;";
+                    SqlConnection = SqlConnection.Replace("[DbName]", item);
+                    Console.WriteLine(string.Format("数据库名称:{0},连接字符串:{1}", item, SqlConnection));
+                    exSql(SqlConnection);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 数据库名称
+        /// </summary>
+        /// <param name="DBName"></param>
+        public void exSql(string SqlConnection)
+        {
+            List<string> listSql = new Program().UpdateSql();
+            List<string> listTable = new DBTableHelper(SqlConnection).GetDbTable();
+            foreach (var item in listTable)
+            {
+                //if (!item.Contains("二手房"))
+                //{
+                //    continue;
+                //}
+
+                List<string> listColumn = new DBTableHelper(SqlConnection).GetTableColumn(item);
+                foreach (var Column in listColumn)
+                {
+                    foreach (var SqlTemp in listSql)
+                    {
+                        try
+                        {
+                            string sql = SqlTemp.Replace("[table]", "[" + item + "]").Replace("[column]", "[" + Column + "]");
+
+                            int Num = new SqlHelper(SqlConnection).ExecuteNonQuery(sql);
+                            if (Num > 0)
+                            {
+                                Console.WriteLine(string.Format("sql:{0},影响行数:{1}", sql, Num));
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(string.Format("表:{2},列:{0}出错,msg:{1}", Column, ex.ToString(), item));
+                        }
+
+                    }
+                }
+            }
+        }
+
+        private void DropColumn(string SqlConnection)
+        {
+            string sqlTemp = "alter table [table] drop column [column]";
+            List<string> listTable = new DBTableHelper(SqlConnection).GetDbTable();
+            foreach (var item in listTable)
+            {
+                List<string> listColumn = new DBTableHelper(SqlConnection).GetTableColumn(item);
+                foreach (var Column in listColumn)
+                {
+                    if (Column.Contains("F"))
+                    {
+                        try
+                        {
+                            string sql = sqlTemp.Replace("[table]", "[" + item + "]").Replace("[column]", "[" + Column + "]");
+                            int Num = new SqlHelper(SqlConnection).ExecuteNonQuery(sql);
+                            Console.WriteLine(string.Format("sql:{0},影响行数:{1}", sql, Num));
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(string.Format("表:{2},列:{0}出错,msg:{1}", Column, ex.ToString(), item));
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
